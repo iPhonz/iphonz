76c11b1d99db3c2f1edbc58687ac61722075b48a
Phonz <119698504+iPhonz@users.noreply.github.com>
2025-04-06 23:47:06 -0700

    Update Post model to include meme images and comments

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -2,16 +2,36 @@ class Post {
   final String username;
   final String profileImage;
   final String content;
+  final String? memeImage; // Optional meme image
   final int likes;
   final int comments;
   final int shares;
+  final String? tagline; // "All we do is skaaaate"
+  final List<Comment> commentsList; // List of comments
+  final bool hasJoined; // Whether the user has joined
 
   Post({
     required this.username,
     required this.profileImage,
     required this.content,
+    this.memeImage,
     required this.likes,
     required this.comments,
     required this.shares,
+    this.tagline,
+    this.commentsList = const [],
+    this.hasJoined = false,
+  });
+}
+
+class Comment {
+  final String username;
+  final String profileImage;
+  final String content;
+
+  Comment({
+    required this.username,
+    required this.profileImage,
+    required this.content,
   });
 }
