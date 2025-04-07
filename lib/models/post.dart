02eb216e58608d91f8ba66c3bb2306f0b9403c95
Phonz <119698504+iPhonz@users.noreply.github.com>
2025-04-07 00:29:54 -0700

    Update Post model to use enhanced Comment model

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,4 +1,7 @@
+import 'comment.dart';
+
 class Post {
+  final String id;
   final String username;
   final String profileImage;
   final String content;
@@ -9,8 +12,10 @@ class Post {
   final String? tagline; // "All we do is skaaaate"
   final List<Comment> commentsList; // List of comments
   final bool hasJoined; // Whether the user has joined
+  final DateTime timestamp;
 
   Post({
+    required this.id,
     required this.username,
     required this.profileImage,
     required this.content,
@@ -21,17 +26,37 @@ class Post {
     this.tagline,
     this.commentsList = const [],
     this.hasJoined = false,
-  });
-}
+    DateTime? timestamp,
+  }) : this.timestamp = timestamp ?? DateTime.now();
 
-class Comment {
-  final String username;
-  final String profileImage;
-  final String content;
-
-  Comment({
-    required this.username,
-    required this.profileImage,
-    required this.content,
-  });
+  // Create a copy of this post with updated values
+  Post copyWith({
+    String? id,
+    String? username,
+    String? profileImage,
+    String? content,
+    String? memeImage,
+    int? likes,
+    int? comments,
+    int? shares,
+    String? tagline,
+    List<Comment>? commentsList,
+    bool? hasJoined,
+    DateTime? timestamp,
+  }) {
+    return Post(
+      id: id ?? this.id,
+      username: username ?? this.username,
+      profileImage: profileImage ?? this.profileImage,
+      content: content ?? this.content,
+      memeImage: memeImage ?? this.memeImage,
+      likes: likes ?? this.likes,
+      comments: comments ?? this.comments,
+      shares: shares ?? this.shares,
+      tagline: tagline ?? this.tagline,
+      commentsList: commentsList ?? this.commentsList,
+      hasJoined: hasJoined ?? this.hasJoined,
+      timestamp: timestamp ?? this.timestamp,
+    );
+  }
 }
